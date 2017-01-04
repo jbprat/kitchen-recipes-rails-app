ac4783a386c830e4d4df12fac061711519f5903c
Jean-Baptiste <jbprat-espouey@academos.qc.ca>
2017-01-03 19:56:44 -0500

    feature/improve-puma-config

--- config/puma.rb
+++ config/puma.rb
@@ -23,25 +23,29 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory. If you use this option
-# you need to make sure to reconnect any threads in the `on_worker_boot`
-# block.
-#
-# preload_app!
+# Preload the app
+preload_app!
 
-# The code in the `on_worker_boot` will be called if you are using
-# clustered mode by specifying a number of `workers`. After each worker
-# process is booted this block will be run, if you are using `preload_app!`
-# option you will want to use this block to reconnect to any threads
-# or connections that may have been created at application boot, Ruby
-# cannot share connections between processes.
-#
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-# end
+# Run code when a worker is spawned
+on_worker_boot do
+  # Set a global logger
+  Rails.logger = ActiveSupport::Logger.new(STDOUT)
+
+  # Set ActiveRecord config
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecordConfigurationOverride.override!
+  end
+
+  # Set ActionController config
+  ActiveSupport.on_load(:action_controller) do
+    ActionController::Base.logger = Rails.logger
+  end
+
+  # Set ActionView config
+  ActiveSupport.on_load(:action_view) do
+    ActionView::Base.logger = Rails.logger
+  end
+end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
